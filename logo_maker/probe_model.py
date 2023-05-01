0fa934c89b8512ae8f6985a42e6d4a477ccf1793
Stephan Majert <stephan.majert@gmail.com>
2023-05-01 10:09:15 +0200

    Add sample stage of diffusion model to pipeline

--- logo_maker/probe_model.py
+++ logo_maker/probe_model.py
@@ -1,82 +1,78 @@
+import argparse
 from pathlib import Path
 
-from gooey import Gooey, GooeyParser
+#from gooey import Gooey, GooeyParser
 from matplotlib import pyplot as plt
 import torch
 
-from logo_maker.data_loading import show_image_grid, ClusterNamesAeGrayscale
+from logo_maker.autoencoder import AutoEncoder
+from logo_maker.data_loading import show_image_grid
 from logo_maker.denoising_diffusion import Generator, draw_sample_from_generator, VarianceSchedule
+import logo_maker.params as params
 
 
-def probe_model(model_file: Path | ClusterNamesAeGrayscale, seed: int,  n_samples: int, device: str) -> None:
+def probe_model(seed: int,  n_samples: int, device: str, save_as: Path | None = None) -> None:
     """
     Sample images from a chosen model.
 
-    :param model_file: Model file (*.pt) or name of a prepared cluster corresponding to a pretrained model
-        in 'LogoMaker/data/model' to draw a sample from.
     :param seed: Random seed to start the generation process.
     :param n_samples: Amount of images to draw from the model.
     :param device: Device to use to run the model. Either 'cuda' or 'cpu'.
     """
-    if isinstance(model_file, ClusterNamesAeGrayscale):
-        model_file = Path(__file__).parents[1] / f"data/models/{model_file.name}/model.pt"
 
-    place_holder_variance_schedule = VarianceSchedule()
-    model = Generator(place_holder_variance_schedule)
-    model = model.to(device)
-    model.load_state_dict(torch.load(model_file))
-    model.eval()
+    autoencoder_file = params.OUTS_BASE_DIR / f"train_autoencoder/model.pt"
+    autoencoder = AutoEncoder()
+    autoencoder.load_state_dict(torch.load(autoencoder_file))
+    autoencoder.eval()
+    autoencoder.to(device)
 
-    batch = draw_sample_from_generator(model, (n_samples, 3, 32, 32), seed=seed)
+    generator_file = params.OUTS_BASE_DIR / f"train_diffusion_model/model.pt"
+    variance_schedule = VarianceSchedule(
+        (params.DiffusionModelParams.VAR_SCHEDULE_START, params.DiffusionModelParams.VAR_SCHEDULE_END),
+        params.DiffusionModelParams.DIFFUSION_STEPS
+    )
+    generator = Generator(variance_schedule, params.DiffusionModelParams.EMBEDDING_DIMENSION)
+    generator.load_state_dict(torch.load(generator_file))
+    generator = generator.to(device)
+    generator.eval()
+
+    batch = draw_sample_from_generator(generator, autoencoder, (n_samples, 3, 8, 8), seed=seed)
     show_image_grid(batch)
+    if save_as is not None:
+        plt.savefig(save_as)
+    else:
+        plt.savefig(params.OUTS_BASE_DIR / "samples.png")
     plt.show()
 
 
-@Gooey(
-    program_name='LogoMaker',
-    image_dir=Path(__file__).parents[1] / "data/gooey_image_dir"
-)
+# @Gooey(
+#     program_name='LogoMaker',
+#     image_dir=params.DATA_BASE_DIR / "gooey_image_dir"
+# )
 def main():
-    parser = GooeyParser(description="Get sample images from model")
+    parser = argparse.ArgumentParser(description="Get sample images from model")
     parser.add_argument(
-        "model",
-        choices=[cluster.name for cluster in ClusterNamesAeGrayscale],
-        default=ClusterNamesAeGrayscale.round_on_white.name,
-        help="Model to use."
+        "--seed", type=int, default=42, help="Random number seed to generate Gaussian noise (first timestep) from."
     )
-    parser.add_argument(
-        "seed", type=int, default=42, help="Random number seed to generate Gaussian noise (first timestep) from."
-    )
-    parser.add_argument("n_samples", type=int, default=1, help="Number of samples to get from model.")
-
-    parser.add_argument(
-        "--custom_model_file",
-        type=Path,
-        default=None,
-        widget="FileChooser",
-        help=(
-            "If you want to use a custom model file (*.pt) instead of the one chosen under the 'model' setting,"
-            " select it here."
-        )
-    )
-
+    parser.add_argument("--n_samples", type=int, default=1, help="Number of samples to get from model.")
     parser.add_argument(
         "--use_gpu",
         help="Try to calculate on GPU",
         action="store_true"
     )
+    parser.add_argument(
+        "--save_as",
+        help="Storage location for result",
+        type=Path,
+        default=None
+    )
 
     args = parser.parse_args()
-    model = args.custom_model_file
-    if model is None:
-        model = ClusterNamesAeGrayscale[args.model]
-
     if args.use_gpu:
         device = "cuda"
     else:
         device = "cpu"
-
-    probe_model(model, args.seed, args.n_samples, device)
+    probe_model(args.seed, args.n_samples, device, save_as=args.save_as)
 
 
 if __name__ == "__main__":
